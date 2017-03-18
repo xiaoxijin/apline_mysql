09890b17cf12acfa27107e618f1285314dc629e7
xiaoxijin <xiaoxijin23@126.com>
2017-03-18 19:03:48 +0800

    测试删除。

--- install.sh
+++ install.sh
@@ -1,20 +1,22 @@
-
-/usr/bin/mysql_install_db --user=mysql
-
-/usr/bin/mysqld_safe &
-
-
-
-
 apk add mysql-client
-/usr/bin/mysqladmin -u root password 'root1234'
-/usr/bin/mysql -uroot -proot1234 < /root/init.sql
-
-
-ps -ef | grep mysqld_safe | grep -v grep  |awk '{print $1}' | xargs kill -9
-ps -ef | grep mysqld | grep -v grep  |awk '{print $1}' | xargs kill -9
-
 apk del mysql-client
+
+#/usr/bin/mysql_install_db --user=mysql
+#
+#/usr/bin/mysqld_safe &
+#
+#
+#
+#
+#
+#/usr/bin/mysqladmin -u root password 'root1234'
+#/usr/bin/mysql -uroot -proot1234 < /root/init.sql
+#
+#
+#ps -ef | grep mysqld_safe | grep -v grep  |awk '{print $1}' | xargs kill -9
+#ps -ef | grep mysqld | grep -v grep  |awk '{print $1}' | xargs kill -9
+
+
 
 
 
