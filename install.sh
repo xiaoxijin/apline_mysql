9e9c90a072755e3930a9faa71cb15022cbc7fe3f
xiaoxijin <xiaoxijin23@126.com>
2017-03-23 11:54:39 +0800

    回复mysql-client

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ chmod 777 -R /work
 
 /usr/bin/mysqld_safe &
 
-
+apk add mysql-client
+#不能删除mysql-client
 /usr/bin/mysqladmin -u root password 'root1234'
 
 /usr/bin/mysql -uroot -proot1234 < /work/init.sql
@@ -14,7 +15,7 @@ chmod 777 -R /work
 ps -ef | grep mysqld_safe | grep -v grep  |awk '{print $1}' | xargs kill -9
 ps -ef | grep mysqld | grep -v grep  |awk '{print $1}' | xargs kill -9
 
-apk del mysql-client
+
 rm -rf /var/cache/apk/*
 
 
